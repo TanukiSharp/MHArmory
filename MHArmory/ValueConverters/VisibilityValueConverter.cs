744318921ef17cbdbe362cbd46c713fbc5055906
Sebastien ROBERT <sebastien.saigo@gmail.com>
2018-07-16 19:09:50 +0900

    Added HiddenVisibilityValueConverter type

--- MHArmory/ValueConverters/VisibilityValueConverter.cs
+++ MHArmory/ValueConverters/VisibilityValueConverter.cs
@@ -9,6 +9,22 @@ using System.Windows.Data;
 
 namespace MHArmory.ValueConverters
 {
+    public class HiddenVisibilityValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b && b)
+                return Visibility.Visible;
+
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     public class CollapsedVisibilityValueConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
