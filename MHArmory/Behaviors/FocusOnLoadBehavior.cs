2d382ebd158991d80902078538aa8d091fe7f4c8
Sebastien ROBERT <sebastien.saigo@gmail.com>
2018-07-31 00:18:35 +0900

    Added DoubleClickBehavior and FocusOnLoadBehavior behaviors

---
+++ MHArmory/Behaviors/FocusOnLoadBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MHArmory.Behaviors
+{
+    public static class FocusOnLoadBehavior
+    {
+        public static bool GetIsAttached(FrameworkElement target)
+        {
+            return (bool)target.GetValue(IsAttachedProperty);
+        }
+
+        public static void SetIsAttached(FrameworkElement target, bool value)
+        {
+            target.SetValue(IsAttachedProperty, value);
+        }
+
+        public static readonly DependencyProperty IsAttachedProperty = DependencyProperty.RegisterAttached(
+            "IsAttached",
+            typeof(bool),
+            typeof(FocusOnLoadBehavior),
+            new PropertyMetadata(OnIsAttachedChanged)
+        );
+
+        private static void OnIsAttachedChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var item = (FrameworkElement)sender;
+
+            if (item.IsLoaded)
+                MakeFocused(item);
+            else
+                item.Loaded += ItemLoaded;
+        }
+
+        private static void ItemLoaded(object sender, RoutedEventArgs e)
+        {
+            var item = (FrameworkElement)sender;
+            item.Loaded -= ItemLoaded;
+            MakeFocused(item);
+        }
+
+        private static void MakeFocused(FrameworkElement item)
+        {
+            item.Focus();
+
+            if (item is ListBox listBox)
+            {
+                DependencyObject listItem = listBox.ItemContainerGenerator.ContainerFromIndex(listBox.SelectedIndex);
+
+                if (listItem is ListBoxItem listBoxItem)
+                    listBoxItem.Focus();
+            }
+        }
+    }
+}
