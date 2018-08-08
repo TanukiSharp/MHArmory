00a30a69d05d1ce7183a48964cecc46986f71b1b
Sebastien ROBERT <sebastien.saigo@gmail.com>
2018-08-08 15:58:58 +0900

    Started to rewrite the way search works

---
+++ MHArmory/SearchUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHArmory
+{
+    public struct SearchInfo
+    {
+        public bool IsExact;
+        public string Text;
+    }
+
+    public class SearchText
+    {
+        public SearchText(string searchText)
+        {
+            if (searchText == null)
+                throw new ArgumentNullException(nameof(searchText));
+
+            foreach (string sub in searchText.Split(',', ';', '/', ':').Select(x => x.Trim().ToLower()))
+            {
+
+            }
+        }
+    }
+
+    public class SearchUtilities
+    {
+    }
+}
