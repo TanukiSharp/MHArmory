8415562fb8e9933d788601ff7f4286108c02c0e4
Sebastien ROBERT <sebastien.saigo@gmail.com>
2018-08-08 15:59:28 +0900

    Started to implement jewel ooverride feature (underway)

---
+++ MHArmory/ViewModels/JewelOverrideViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MHArmory.Core.DataStructures;
+
+namespace MHArmory.ViewModels
+{
+    public class JewelOverrideViewModel : ViewModelBase
+    {
+        private readonly IJewel jewel;
+
+        public string Name { get; }
+        public int SlotSize { get; }
+        public IAbility[] Abilities { get; }
+
+        private int count;
+        public int Count
+        {
+            get { return count; }
+            set { SetValue(ref count, value); }
+        }
+
+        private bool isOverriding;
+        public bool IsOverriding
+        {
+            get { return isOverriding; }
+            set { SetValue(ref isOverriding, value); }
+        }
+
+        public JewelOverrideViewModel(IJewel jewel, int count)
+        {
+            Name = jewel.Name;
+            SlotSize = jewel.SlotSize;
+            Abilities = jewel.Abilities;
+        }
+    }
+}
