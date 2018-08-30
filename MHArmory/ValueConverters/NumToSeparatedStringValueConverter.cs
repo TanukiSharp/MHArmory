acebe5fef190bd686b673d7000c87e3bb386742b
Sebastien ROBERT <sebastien.saigo@gmail.com>
2018-08-30 19:16:16 +0900

    Added NumToSeparatedStringValueConverter for future use

---
+++ MHArmory/ValueConverters/NumToSeparatedStringValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MHArmory.ValueConverters
+{
+    public class NumToSeparatedStringValueConverter : IValueConverter
+    {
+        private static readonly NumberFormatInfo nfi = new NumberFormatInfo
+        {
+            NumberGroupSeparator = "'"
+        };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ChangeType(value, typeof(string), nfi);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
