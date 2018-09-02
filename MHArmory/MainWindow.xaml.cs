4ea3378dd190807a76f8501cabd898fcfc4388ca
Sebastien ROBERT <sebastien.saigo@gmail.com>
2018-09-02 21:13:07 +0900

    Added dirty test code to help find out discrepancies across data sources

--- MHArmory/MainWindow.xaml.cs
+++ MHArmory/MainWindow.xaml.cs
@@ -88,15 +88,135 @@ namespace MHArmory
             GlobalData.Instance.Configuration = configuration;
         }
 
+        public class LambdaEqualityComparer<T> : IEqualityComparer<T>
+        {
+            private readonly Func<T, T, bool> equals;
+            private readonly Func<T, int> getHashCode;
+
+            public LambdaEqualityComparer(Func<T, T, bool> equals)
+                : this(equals, _ => 0)
+            {
+            }
+
+            public LambdaEqualityComparer(Func<T, T, bool> equals, Func<T, int> getHashCode)
+            {
+                this.equals = equals;
+                this.getHashCode = getHashCode;
+            }
+
+            public bool Equals(T x, T y)
+            {
+                return equals(x, y);
+            }
+
+            public int GetHashCode(T obj)
+            {
+                return getHashCode(obj);
+            }
+        }
+
+        public static int ComputeDamereauLevensheinDistance(string s, string t)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                if (string.IsNullOrWhiteSpace(t))
+                    return 0;
+
+                return t.Length;
+            }
+
+            if (string.IsNullOrWhiteSpace(t))
+                return s.Length;
+
+            int n = s.Length;
+            int m = t.Length;
+
+            int[,] d = new int[n + 1, m + 1];
+
+            // initialize the top and right of the table to 0, 1, 2, ...
+            for (int i = 0; i <= n; d[i, 0] = i++)
+                ;
+            for (int j = 1; j <= m; d[0, j] = j++)
+                ;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                    int min1 = d[i - 1, j] + 1;
+                    int min2 = d[i, j - 1] + 1;
+                    int min3 = d[i - 1, j - 1] + cost;
+
+                    d[i, j] = Math.Min(Math.Min(min1, min2), min3);
+                }
+            }
+
+            return d[n, m];
+        }
+
         private async Task LoadData()
         {
-            //var source = new MhwDbDataSource.DataSource(null, App.HasWriteAccess);
+            //var source2 = new MhwDbDataSource.DataSource(null, App.HasWriteAccess);
+            //ISkill[] skills2 = await source2.GetSkills();
+            //IArmorPiece[] armors2 = await source2.GetArmorPieces();
+            //ICharm[] charms2 = await source2.GetCharms();
+            //IJewel[] jewels2 = await source2.GetJewels();
+
+            //// --------------------------------------------------
+
             var source = new AthenaAssDataSource.DataSource();
 
             ISkill[] skills = await source.GetSkills();
             IArmorPiece[] armors = await source.GetArmorPieces();
             ICharm[] charms = await source.GetCharms();
             IJewel[] jewels = await source.GetJewels();
+
+
+
+            ///***********************************************************************/
+
+            //ISkill[] testSkills = skills.Except(skills2, new LambdaEqualityComparer<ISkill>((x, y) => x.Name == y.Name)).ToArray();
+            //IArmorPiece[] testArmors = armors.Except(armors2, new LambdaEqualityComparer<IArmorPiece>((x, y) => x.Name == y.Name)).ToArray();
+
+            //foreach (IArmorPiece a2 in armors2)
+            //{
+            //    if (armors.Any(a1 => a1.Name == a2.Name))
+            //        continue;
+
+            //    IArmorPiece[] bestByNameDistance = armors
+            //        .Select(a1 => new { Armor = a1, Distance = ComputeDamereauLevensheinDistance(a1.Name, a2.Name) })
+            //        .OrderBy(x => x.Distance)
+            //        .Select(x => x.Armor)
+            //        .ToArray();
+
+            //    if (bestByNameDistance[0].Name != a2.Name)
+            //    {
+            //    }
+
+            //    Console.WriteLine(bestByNameDistance[0].Name);
+            //}
+
+            //foreach (IArmorPiece a1 in armors)
+            //{
+            //    if (armors2.Any(a2 => a2.Name == a1.Name))
+            //        continue;
+
+            //    IArmorPiece[] bestByNameDistance = armors2
+            //        .Select(a2 => new { Armor = a2, Distance = ComputeDamereauLevensheinDistance(a2.Name, a1.Name) })
+            //        .OrderBy(x => x.Distance)
+            //        .Select(x => x.Armor)
+            //        .ToArray();
+
+            //    if (bestByNameDistance[0].Name != a1.Name)
+            //    {
+            //    }
+
+            //    Console.WriteLine(bestByNameDistance[0].Name);
+            //}
+
+            /***********************************************************************/
+
 
             if (skills == null)
             {
