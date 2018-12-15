38cdaca6c5ff8f15c2d47672f30dfc9750d56763
Sebastien ROBERT <sebastien.saigo@gmail.com>
2018-12-15 15:50:59 +0900

    Minor change, ensure IsEnabled in AutoUpdateLogger will not throw

--- MHArmory/ViewModels/AutoUpdateViewModel.cs
+++ MHArmory/ViewModels/AutoUpdateViewModel.cs
@@ -100,7 +100,7 @@ namespace MHArmory.ViewModels
 
             public bool IsEnabled(LogLevel logLevel)
             {
-                throw new NotImplementedException();
+                return true;
             }
 
             public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
