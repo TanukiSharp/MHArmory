f443a0539e15cec7470b225cebc0463c5ef79678
gediminas.masaitis@gmail.com <ksJ3i',q~hS['sx\C{vT>
2019-01-06 22:48:32 +0200

    Fixed incorrect class name, forgot to commit the file, whoops

--- MHArmory/ViewModels/RootViewModel.cs
+++ MHArmory/ViewModels/RootViewModel.cs
@@ -357,7 +357,7 @@ namespace MHArmory.ViewModels
                 .Select(x => x.Ability)
                 .ToList();
 
-            SolverData = new SolverData2(
+            SolverData = new IncrementalSolverData(
                 InParameters.Slots.Select(x => x.Value).ToList(),
                 GlobalData.Instance.Heads.Where(x => ArmorPieceMatchInParameters(x)),
                 GlobalData.Instance.Chests.Where(x => ArmorPieceMatchInParameters(x)),
